a30f7e7e730141fcb4142d188cdd58c8d693f277
jessie <sheepyang2003@hotmail.com>
2025-10-29 20:01:51 -0400

    Update User.py
    Added a login UI and integrated it with the existing functionality

--- Sprint 1/User.py
+++ Sprint 1/User.py
@@ -1,6 +1,8 @@
 # User.py
-# Handles login functionality
+# Handles login functionality and UI
 
+import customtkinter as ctk
+import tkinter.messagebox as tkmb
 from DataStorage import DataStorage
 
 class User:
@@ -8,9 +10,39 @@ class User:
         self.storage = storage
         self.logged_in_user = None
 
-    def login(self, email, password):
+    def login(self):
+        email = self.user_entry.get()
+        password = self.user_pass.get()
+
         if self.storage.validate_login(email, password):
             self.logged_in_user = email
-            return "Login successful"
+            tkmb.showinfo(title="Login Successful",message="You have logged in successfully")
         else:
-            return "Error: Invalid email or password"
+            tkmb.showerror(title="Login Failed",message="Invalid email and/or password")
+
+    def create_login_ui(self):
+        ctk.set_appearance_mode("light")
+
+        app = ctk.CTk()
+        app.geometry("400x400")
+        app.title("Baddie Health Tracker Login Page")
+
+        label = ctk.CTkLabel(app,text="Baddie Health Tracker",font=ctk.CTkFont(family="Gabriola",size=20,weight="bold"))
+        label.pack(pady=20)
+
+        frame = ctk.CTkFrame(master=app,fg_color="#FFD1DC",border_color="FFE3EA")
+        frame.pack(pady=20,padx=40,fill="both",expand=True)
+
+        label = ctk.CTkLabel(master=frame,text='Login')
+        label.pack(pady=12,padx=10)
+
+        self.user_entry = ctk.CTkEntry(master=frame,placeholder_text="Email")
+        self.user_entry.pack(pady=12,padx=10)
+
+        self.user_pass = ctk.CTkEntry(master=frame,placeholder_text="Password",show="*")
+        self.user_pass.pack(pady=12,padx=10)
+
+        button = ctk.CTkButton(master=frame,fg_color="#997D84",text='Login',hover=False,command=self.login)
+        button.pack(pady=12,padx=10)
+
+        app.mainloop()
